93c318f75e00d13621e08bbd15d41fa0c6da277e
Hamza Safraou <hamza.safraou@esprit.tn>
2021-11-10 21:59:33 +0100

    added employee test

---
+++ src/test/java/tn/esprit/spring/services/EmployeServiceImplTest.java
@@ -0,0 +1,29 @@
+package tn.esprit.spring.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.services.IEmployeService;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeServiceImplTest {
+    @Autowired
+    IEmployeService ems;
+    @Test
+    public void ajouterEmploye() {
+        Employe e = new Employe("hamza","safraou","hamza.safraou@esprit.tn",true,Role.CHEF_DEPARTEMENT);
+        assertTrue(e.getRole().equals(Role.CHEF_DEPARTEMENT));
+        ems.ajouterEmploye(e);
+    }
+    @Test
+    public void mettreAjourEmailByEmployeIdTest() {
+        ems.mettreAjourEmailByEmployeId("hamza_safraou@esprit.tn",1);
+	}
+}
